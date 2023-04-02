a4848756c051e2088118e7a7b6d43d0147f049e2
Fredrick Makoffu <fredrickmakoffu@gmail.com>
2023-04-02 18:04:32 +0300

    Added action in company store to retrieve company symbol options. Loaded the action in the FormComponent component

--- resources/js/components/FormComponent.vue
+++ resources/js/components/FormComponent.vue
@@ -1,3 +1,28 @@
 <template>
     <h2>Form</h2>
 </template>
+
+
+<script>
+    import { mapActions, mapGetters } from 'vuex'
+
+    export default {
+        name: 'FormComponent',
+        data() {
+            return {
+                name: '',
+                email: '',
+                message: '',
+                errors: []
+            }
+        },
+        mounted() {
+            this.fetchCompanies()
+        },
+        methods: {
+            ...mapActions({
+                fetchCompanies: 'company/fetchCompanies'
+            })
+        }
+    }
+</script>
--- resources/js/store/company.store.js
+++ resources/js/store/company.store.js
@@ -1,3 +1,28 @@
-export default {
+import api from '../api';
 
+const headers =  {
+    "Accept": "application/json"
+}
+
+export default {
+    namespaced: true,
+    state: {
+        companies: []
+    },
+    mutations: {
+        setCompanies(state, companies) {
+            state.companies = companies;
+        }
+    },
+    actions: {
+        async fetchCompanies({ commit }) {
+            const response = await api.get('https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_json/data/a5bc7580d6176d60ac0b2142ca8d7df6/nasdaq-listed_json.json', null, headers);
+            
+            console.log(response)
+            commit('setCompanies', response);
+        }
+    },
+    getters: {
+        companies: state => state.companies
+    }
 }
